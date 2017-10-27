96ac9a40bc3bb14a35f6205401ffe6d12809ab00
uw <uw@colo113.tuthost.com>
2017-10-27 21:57:05 +0300

    21102017

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,3 +1,83 @@
 <?php
 $this->title = 'Главная';
 ?>
+<div class="front">
+    <div class="container_inner top_block">
+        <img class="tass_logo" src="/frontend/web/images/tass_logo.png"/>
+        <div class="social">
+            <a class="fb" href="#"></a>
+            <a class="vk" href="#"></a>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <div class="container_inner front_text">
+    	<img class="logo_big" src="/frontend/web/images/logo_big.png"/>
+        <h1>Реновации москвы.<br/> История, Герои, география</h1>
+    </div>
+    <div class="grid_wrap">
+        <div class="grid_front">
+            <div class="grid_item map">
+                <img src="/frontend/web/images/map_img_front.jpg"/>
+                <div class="text_info">
+                    <div class="small">Карта</div>
+                    <a href="#">Стартовые площадки 1 этапа реновации</a>
+                </div>
+            </div>
+            <div class="grid_item text1">
+                <div class="block_text">
+                    <div class="small">Читать</div>
+                    <div class="text">
+                        <a href="#">История реноваций: даты и факты</a>
+                    </div>
+                </div>
+            </div>
+            <div class="grid_item article">
+                <img src="/frontend/web/images/article_img_front.jpg"/>
+                <div class="text_info">
+                    <div class="small">Читать</div>
+                    <a href="#">Герои: личные истории</a>
+                </div>
+            </div>
+            <div class="grid_item text2">
+                <div class="part part1">
+                    <div class="block_text">
+                        <div class="small">Закон</div>
+                        <div class="text">
+                            <a href="#">Расшифровка внутри</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="part part2">
+                    <div class="block_text">
+                        <div class="small">Таймлайн</div>
+                        <div class="text">
+                            <a href="#">Даты реализации проекта по реновации</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid_item video">
+                <a href="#" class="bottom_video"><img src="/frontend/web/images/video_front_img.jpg"/></a>
+                <iframe width="100%" height="790" src="https://www.youtube.com/embed/ZStqzGWEBGw" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="grid_item slider_front_item">
+                <div class="slider_front_wrap">
+                    <div class="slider_front owl-carousel">
+                        <div class="slider_front_item">
+                            <a href="#"><img src="/frontend/web/images/slider_front_img.jpg"/></a>
+                        </div>
+                        <div class="slider_front_item">
+                            <a href="#"><img src="/frontend/web/images/slider_front_img.jpg"/></a>
+                        </div>
+                        <div class="slider_front_item">
+                            <a href="#"><img src="/frontend/web/images/slider_front_img.jpg"/></a>
+                        </div>
+                        <div class="slider_front_item">
+                            <a href="#"><img src="/frontend/web/images/slider_front_img.jpg"/></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- frontend/web/js/uw.js
+++ frontend/web/js/uw.js
@@ -1,10 +1,21 @@
 $(window).load(function(){
 	if ($('.slider_gallery').length > 0) {
 		$(".slider_gallery").owlCarousel({
-			items: 1
+			items: 1,
+			loop: true
 		});
 	}
 	$('.grid').masonry({
+	  itemSelector: '.grid_item',
+	});
+	if ($('.slider_front').length > 0) {
+		$(".slider_front").owlCarousel({
+			items: 1,
+			loop: true,
+			nav: true
+		});
+	}
+	$('.grid_front').masonry({
 	  itemSelector: '.grid_item',
 	});
 	$(".gallery_inline").colorbox({inline:true, width:"66%"});
@@ -18,6 +29,13 @@ $(window).load(function(){
 	    console.log('111');
 	    $(this).closest('.bottom_gallery_img').find('iframe').attr('src', videoSRC.split('?')[0] + '?rel=0&autoplay=1');
 	    $(this).closest('.bottom_gallery_img').find('iframe').fadeIn('slow');
+	    return false;
+	    //$(this).parent().parent().parent().after('<div class="close-video"></div>');
+	});
+	$('body').on('click', '.bottom_video', function(){
+	    var videoSRC = $(this).closest('.video').find('iframe').attr('src');
+	    $(this).closest('.video').find('iframe').attr('src', videoSRC.split('?')[0] + '?rel=0&autoplay=1');
+	    $(this).closest('.video').find('iframe').fadeIn('slow');
 	    return false;
 	    //$(this).parent().parent().parent().after('<div class="close-video"></div>');
 	});

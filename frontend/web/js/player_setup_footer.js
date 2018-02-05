c51d973a349d65affc7bb7f9fbb42ac8a0591136
Nick <pyatakn@gmail.com>
2018-02-05 19:43:18 +0300

    upd

--- frontend/web/js/player_setup_footer.js
+++ frontend/web/js/player_setup_footer.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             'autostart': 'false',
             'duration': '',
             'playlist': [{
-                'image': '/frontend/web/images/footer_gallery_img_new.jpg',
+                'image': '/images/footer_gallery_img_new.jpg',
                 'sources': [
                     {file: "https://renovation-vod.tass.ru/renovation-vod/smil:slideshow1.smil/jwplayer.smil"},
                     {file: "https://renovation-vod.tass.ru/renovation-vod/smil:slideshow1.smil/playlist.m3u8"},
